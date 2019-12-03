bb74143cb2fd5a8de7f3c51ec99881630d8374ba
VinhNguyen <nhvinh604@gmail.com>
2019-12-03 18:21:18 +0700

    re-config sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,24 +1,24 @@
 const dotenv = require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "data123",
-    "database": "DB_WinterShop",
-    "host": "localhost",
+    "username": "bvfytdwxrpbqee",
+    "password": "83402e20c3511b33b5ba022ce629d9367400638569a368596b2c193cd86c3628",
+    "database": "dfq7n642cgfjnk",
+    "host": "ec2-174-129-255-69.compute-1.amazonaws.com",
     "dialect": "postgres",
   },
   "test": {
-    "username": "postgres",
-    "password": "data123",
-    "database": "DB_WinterShop",
-    "host": "localhost",
+    "username": "bvfytdwxrpbqee",
+    "password": "83402e20c3511b33b5ba022ce629d9367400638569a368596b2c193cd86c3628",
+    "database": "dfq7n642cgfjnk",
+    "host": "ec2-174-129-255-69.compute-1.amazonaws.com",
     "dialect": "postgres",
   },
   "production": {
-    "username": "postgres",
-    "password": "data123",
-    "database": "DB_WinterShop",
-    "host": "localhost",
+    "username": "bvfytdwxrpbqee",
+    "password": "83402e20c3511b33b5ba022ce629d9367400638569a368596b2c193cd86c3628",
+    "database": "dfq7n642cgfjnk",
+    "host": "ec2-174-129-255-69.compute-1.amazonaws.com",
     "dialect": "postgres",
   }
 };
